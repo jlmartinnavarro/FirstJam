a322533df92506a8769cb7c317bb2fb9d3e6f0e9
JL <jlmartinnavarro@hotmail.com>
2018-07-17 18:02:04 +0200

    Mensajes En Discord

---
+++ Normandia/Assets/Scripts/GameOver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+    public GameObject gameOver;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == SceneController.enemyTag)
+        {
+            //Game OVer
+            Debug.Log("GAME OVER");
+            gameOver.SetActive(true);
+        }
+    }
+
+
+}
